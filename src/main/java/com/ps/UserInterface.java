9c008fa73c2c903a1cb254261f22817c41670c88
Zirmith <f2v74yt@gmail.com>
2024-11-01 14:34:44 -0500

    first commit

---
+++ src/main/java/com/ps/UserInterface.java
@@ -0,0 +1,18 @@
+package com.ps;
+
+public class UserInterface {
+    // private static Dealership dealership;
+    private static Dealership dealership;
+
+    public static void init(){
+        //Loading the dealership and inventory from a file
+        dealership = DealershipFileManager.getDealership();
+    }
+    public static void display(){
+        init();
+
+        System.out.println("Load the menu");
+        // Load the menu
+    }
+
+}
